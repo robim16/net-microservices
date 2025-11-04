885fc7b13787fdca3fc17b2256abb6433c981e43
robim16 <carteagajimenez@gmail.com>
2025-11-04 16:05:00 -0500

    editar items method

--- src/Play.Catalog.Service/obj/Debug/net5.0/Play.Catalog.Service.AssemblyInfo.cs
+++ src/Play.Catalog.Service/obj/Debug/net5.0/Play.Catalog.Service.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Play.Catalog.Service")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+69146a16ca39e7a83a888d10076fbb60a4c91c94")]
 [assembly: System.Reflection.AssemblyProductAttribute("Play.Catalog.Service")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Play.Catalog.Service")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
