db651f618834722a524c467d80af7847b8f482bd
robim16 <carteagajimenez@gmail.com>
2025-11-17 16:01:08 -0500

    cambios en items controller

--- src/Play.Catalog.Service/obj/Debug/net5.0/Play.Catalog.Service.AssemblyInfo.cs
+++ src/Play.Catalog.Service/obj/Debug/net5.0/Play.Catalog.Service.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Play.Catalog.Service")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f034ec729db4983122fe0565494cd28b125d98b5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+33d4b052e3c543e031d02e23f501862b25a94294")]
 [assembly: System.Reflection.AssemblyProductAttribute("Play.Catalog.Service")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Play.Catalog.Service")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
